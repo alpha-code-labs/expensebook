4ffb2340bf7841b328b8b02ea8f54e9947293c9d
Anandhu <anandhuashok.k@gmail.com>
2023-09-13 10:52:42 +0530

    Backend server setup completed , Connected to database , Database Schema created for Hr & Company Structure container, Account line container schema completed, using tenentId from Hr & Compant Strucutre container you can create accountline for the companty

---
+++ server/model/account_line.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose"
+
+// Import the HRCompany model
+import HRCompany from "./hr_company_srtucture.js"
+
+//  accountline schema
+const accountlineSchema = new mongoose.Schema({
+  tenantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  accountLines: [{
+    // Define the structure of each accountLine item here
+    //  fields like name, balance, etc.
+    name: String,
+    balance: Number,
+    // Other fields for accountLine
+  }],
+})
+
+const Accountline = mongoose.model('Accountline', accountlineSchema);
+
+// Function to create an accountline document with the tenantId
+async function createAccountlineWithTenantId(tenantId, accountLineData) {
+    try {
+      // Find the HRCompany document by its tenantId
+      const hrCollection = await HRCompany.findOne({ tenantId: tenantId });
+  
+      if (!hrCollection) {
+        throw new Error('HRCompany document not found');
+      }
+  
+      // Create a new accountline document with the obtained tenantId
+      const accountline = new Accountline({
+        tenantId: hrCollection.tenantId, // 
+        accountLines: [accountLineData],
+      });
+  
+      // Save the newly created accountline document
+      const savedAccountline = await accountline.save();
+  
+      return savedAccountline;
+    } catch (error) {
+      console.error('Error creating accountline:', error);
+      throw error;
+    }
+  }
+  
+
+// Export the Accountline model and the createAccountlineWithTenantId function
+export { Accountline, createAccountlineWithTenantId }
