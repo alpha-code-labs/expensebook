b95d33f750cd583a1142b9be206e79a8ecb09d05
Ankit Singh <ajayaxes318@gmail.com>
2023-09-27 15:53:06 +0530

    added cabin font in font family config section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cabin': ['Cabin', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
