698aecee63cdcb436242692d49513693c962cf77
Ankit Singh <ajayaxes318@gmail.com>
2023-09-27 16:00:09 +0530

    imported and routed pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState, useEffect, createContext } from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import axios from 'axios'
+
 import "./App.css";
+import Page_1 from './pages/Page_1'
+import Page_2 from './pages/Page_2'
+import Page_3 from './pages/Page_3'
+import Page_4 from './pages/Page_4'
+
+
 
 function App() {
+  
   const TRAVEL_MICROSERVICE_SERVER_URL = "";
   
   //flags
   
-
+/*
   axios
     .get(`${TRAVEL_MICROSERVICE_SERVER_URL}/initial-data`)
     .then((response) => {
@@ -19,17 +26,16 @@ function App() {
       console.err(err)
       //handle possible scenarios
     })
+*/
 
   return <>
     <Router>
       <Routes>
-        <Route path='/travel-request' element={} />
-        <Route path='/travel-request/section0' element={} />
-        <Route path='/travel-request/section1' element={} />
-        <Route path='/travel-request/section2' element={} />
-        <Route path='/travel-request/section3' element={} />
-        <Route path='/travel-request/section4' element={} />
-        <Route path='/travel-request/section5' element={} />
+        <Route path='/' element={<Page_1 />} />
+        <Route path='/section0' element={<Page_1 />} />
+        <Route path='/section1' element={<Page_2 />} />
+        <Route path='/section2' element={<Page_3 />} />
+        <Route path='/section3' element={<Page_4 />} />
       </Routes>
     </Router>
   </>;
