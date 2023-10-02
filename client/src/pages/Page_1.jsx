63f967ed89e7de888af2965f97f28bfe54b0aadf
Ankit Singh <ajayaxes318@gmail.com>
2023-10-02 11:37:05 +0530

    added table to first page

--- client/src/pages/Page_1.jsx
+++ client/src/pages/Page_1.jsx
@@ -1,44 +1,149 @@
 import { useState, useEffect } from 'react'
 import leftArrow_icon from '../assets/arrow-left.svg'
 import Select from '../components/common/Select'
+import ObjectSelect from '../components/custom/page_1/ObjectSelect'
 import MultiSearch from '../components/common/MultiSearch'
-import Search from '../components/common/Search'
+import Search from '../components/custom/page_1/Search'
 import Button from '../components/common/Button'
 import { useNavigate } from 'react-router-dom'
 import Icon from '../components/common/Icon'
 import InputPercentage from '../components/common/InputPercentage'
+import Checkbox from '../components/common/Checkbox'
+import TableItem from '../components/table/TableItem'
 
+const teamMembers2 = [{name: 'Rajat Rathor', empId: '201', designation: 'Executive'}, {name: 'Ashish Pundir', empId: '205', designation: 'Program Planner'}, {name: 'Ankit Pundir', empId:'209', designation:'Manager'}]
 
+//dummy onboarding data...
 const options = ['Meeting with client', 'Sales Trip', 'Business Trip']
-const dummyDelegatedFor = [{name: 'Ajay Singh', empId:'', group:'', EmpRole:'', teamMembers:[]}, {name: 'Abhijay Singh', empId:'', group:'', EmpRole:'', teamMembers:[]}, {name: 'Akshay Kumar', empId:'', group:'', EmpRole:'', teamMembers:[]}, {name:'Anandhu Ashok K.', empId:'', group:'', EmpRole:'', teamMembers:[]}, {name:'kanhaiya', empId:'', group:'', EmpRole:'', teamMembers:[]}]
 
-export default function Page_1(props){
+const dummyDelegatedFor = [
+        {name: 'Ajay Singh', empId:'121', group:'', EmpRole:'', teamMembers:[]}, 
+        {name: 'Abhijay Singh', empId:'124', group:'', EmpRole:'', teamMembers:teamMembers2}, 
+        {name: 'Akshay Kumar', empId:'127', group:'', EmpRole:'', teamMembers:[]}, 
+        {name:'Anandhu Ashok K.', empId:'129', group:'', EmpRole:'', teamMembers:[]}, 
+        {name:'kanhaiya', empId:'', group:'135', EmpRole:'', teamMembers:[]}
+    ]
 
-    const [tripPurpose, setTripPurpose] = useState(null)
-    const APPROVAL_FLAG = props.APPROVAL_FLAG || true
-    const MANAGER_FLAG = props.MANAGER_FLAG || true
-    //can raise request for someone else
-    const DELEGATED_FLAG = props.DELEGATED_FLAG || true
+    //approvals 
+    const APPROVAL_FLAG =  true
+    const listOfAllManagers = [
+        {name: 'Preeti Arora', empId: '005'},
+        {name: 'Abhas Kamboj', empId: '045'},
+        {name: 'Sandeep Nair', empId: '061'}
+    ]
 
     //allocation header 
     const ALLOCATION_HEADER = true
-    const allocationHeaderDepartments = ['Sales', 'Marketing', 'Engineering', 'Research']
-    const [selectedAllocationHeaders, setSelectedAllocationHeaders] = useState([])
+    const travelAllocationHeaders = ['Sales', 'Marketing', 'Engineering', 'Research']
+    
 
+
+export default function Page_1(props){
+
+    //details of current employee
+    
+    const EMPLOYEE_ID  = '123'
+    const MANAGER_FLAG =  false
+    const teamMembers = [{name: 'Aman Bhagel', empId: '204', designation: 'Sales Executive'}, {name: 'Vikas Rajput', empId: '245', designation:'System Engineer II'}, {name: 'Rahul Suyush Singh', empId: '318', designation:'Sr. Software Engineer'}, {name: 'Vilakshan Vibhut Giri Babaji Maharaj', empId: '158', designation:'Sr. Sales Executive'}]
+    
+
+    //trip purpose state variable
+    const [tripPurpose, setTripPurpose] = useState(null)
+    //can raise request for someone else
+    const DELEGATED_FLAG = props.DELEGATED_FLAG || true
     //allowed to raise request for these employees
     const delegatedFor = props.delegatedFor || dummyDelegatedFor
+
+    const [travelRequestCreatedFor, setTravelRequestCreatedFor] = useState([])
+
+    //delegator states
+   
+    const [raisingForDelegator, setRaisingForDelegator] = useState(false)
+    const [nameOfDelegator, setNameOfDelegator] = useState('') 
+    const [isDelegatorManager, setIsDelegatorManager] = useState(false)
+    const [delegatorsTeamMembers, setDelegatorsTeamMembers] = useState([])
+    const [selectDelegatorTeamMembers, setSelectDelegatorTeamMembers] = useState(false)
+    const [approvers, setApprovers] =  useState([])
+
+    useEffect(()=>{
+        if(DELEGATED_FLAG && travelRequestCreatedFor.length>0 && EMPLOYEE_ID != travelRequestCreatedFor[0]){
+            setRaisingForDelegator(true)
+            const delegator = delegatedFor.filter(employee=>employee.empId == travelRequestCreatedFor[0])
+            const nameOfDelegator = delegator[0].name
+            setNameOfDelegator(nameOfDelegator)
+            const teamMembers = delegator[0].teamMembers
+            setDelegatorsTeamMembers(teamMembers)
+            //this logic might not be right if there is a fuckup in integrating the company.
+            //some explicit variable from backend might be needed
+            
+            setIsDelegatorManager(teamMembers && teamMembers.length>0)
+
+            console.log(travelRequestCreatedFor[0])
+            console.log(delegatedFor.filter(employee=>employee.empId == travelRequestCreatedFor[0]))
+            console.log( {nameOfDelegator, teamMembers, isDelegatorManager} )
+        }
+    },[travelRequestCreatedFor])
+
+    useEffect(()=>{
+        setSelectedTeamMembers([])
+    },[selectDelegatorTeamMembers])
+    
+
+    const [selectedTravelAllocationHeaders, setSelectedTravelAllocationHeaders] = useState([])
+
+    //if employee is a manager
     const [bookingForSelf, setBookingForSelf] = useState(true)
     const [bookingForTeam, setBookingForTeam] = useState(false)
+
+    useEffect(()=>{
+        //reset everything 
+        setTravelRequestCreatedFor([])
+        setSelectedTravelAllocationHeaders([])
+
+    },[bookingForTeam])
+
+
+    //team member state and selection method
+    const [selectedTeamMembers, setSelectedTeamMembers] = useState([])
+
+    useEffect(()=>{
+        console.log(selectedTeamMembers)
+    },[selectedTeamMembers])
+
+    const handleTeamMemberSelect = (e, id)=>{
+
+        console.log(id)
+        console.log(selectedTeamMembers.includes(id))
+        
+        if(e.target.checked){
+            if(!selectedTeamMembers.includes(id)){
+                let updatedMembers = selectedTeamMembers.slice()
+                updatedMembers.push(id)
+                setSelectedTeamMembers(updatedMembers)
+            }
+        }
+        else{
+            if(selectedTeamMembers.includes(id)){
+                console.log('this also ran')
+                const index = selectedTeamMembers.indexOf(id)
+                let updatedMembers = selectedTeamMembers.slice()
+                updatedMembers.splice(index,1)
+                console.log(updatedMembers)
+                setSelectedTeamMembers(updatedMembers)
+            }
+        }
+    }
+
     const navigate = useNavigate()
 
     const handleContinueButton = ()=>{
+        console.log(sectionForm)
         navigate('/section1')    
     }
 
     const handleAllocationPercentageChange = (percentage, item)=>{
-        
-        const updatedSelectedAllocationHeaders =  
-        selectedAllocationHeaders.map(prevItem=>{
+        const updatedSelectedTravelAllocationHeaders =  
+        selectedTravelAllocationHeaders.map(prevItem=>{
             if(JSON.stringify(prevItem) == JSON.stringify(item)){
                 console.log('item matches')
                 return{...prevItem, percentage}
@@ -49,16 +154,16 @@ export default function Page_1(props){
             }
         })
 
-        console.log(updatedSelectedAllocationHeaders)
-          setSelectedAllocationHeaders(updatedSelectedAllocationHeaders)
+        console.log(updatedSelectedTravelAllocationHeaders)
+        setSelectedTravelAllocationHeaders(updatedSelectedTravelAllocationHeaders)
     
     }
 
     const handleAllocationHeaderSelect = (option)=>{
         let optionPresent = false
         
-        if(selectedAllocationHeaders && selectedAllocationHeaders.length>0){
-            selectedAllocationHeaders.forEach(item=>{
+        if(selectedTravelAllocationHeaders && selectedTravelAllocationHeaders.length>0){
+            selectedTravelAllocationHeaders.forEach(item=>{
                 if(item.department.toLowerCase() == option.toLowerCase() ){
                     optionPresent=true
                     return
@@ -67,10 +172,20 @@ export default function Page_1(props){
         }
         
         if(!optionPresent){
-            setSelectedAllocationHeaders((pre)=>[...pre, {department:option, percentage:''}])
+            setSelectedTravelAllocationHeaders((pre)=>[...pre, {department:option, percentage:''}])
         }
         
     }
+
+
+    const sectionForm =  {
+        createdBy:EMPLOYEE_ID,
+        createdFor:travelRequestCreatedFor,
+        teamMembers:selectedTeamMembers,
+        travelAllocationHeaders: selectedTravelAllocationHeaders,
+        approvers: approvers
+    }
+
 
 
     return(<>
@@ -107,48 +222,52 @@ export default function Page_1(props){
                         placeholder='Select puropse of trip'
                         options={options}
                         currentOption={tripPurpose}
-                        onSelect = {(option)=> {setTripPurpose(option)}} />
+                        onSelect = {(option)=> {setTripPurpose(option.empId)}} />
                 </div>
 
 
                 <div className='mt-8 flex gap-8 flex-wrap'>
                     {/* Booking for.. will be displayed if employee is delegated  */}
-                    {DELEGATED_FLAG && <Search
-                        options={delegatedFor.map(employee=>employee.name)}
+                    {DELEGATED_FLAG && <ObjectSelect
+                        options={delegatedFor}
                         placeholder='Name of the travelling employeee' 
+                        onSelect={(option)=>setTravelRequestCreatedFor([option.empId])}
                         title='Assign request for' />}
 
                     {/* Select approvers */}
-                    {APPROVAL_FLAG && <MultiSearch
+                    {APPROVAL_FLAG && 
+                    <Search
+                        options={listOfAllManagers}
+                        onSelect = {(option)=>{setApprovers(option)}}
                         placeholder='Name of manager approving this' 
                         title='Who will Approve this?' />}
                 </div>
                 <hr className='my-8' />
                 
 
-                {/* allocating budget... will be displayed if allocation headers are present */}
+                {/* allocating travel budget... will be displayed if travel allocation headers are present */}
                 { ALLOCATION_HEADER && <div>
                     <p className='text-base font-medium text-neutral-700 font-cabin'>Select responsible departments for this journey.</p>
                     <div className='mt-8'>
                         <Select
-                            options={allocationHeaderDepartments}
+                            options={travelAllocationHeaders}
                             onSelect = {(option)=>{handleAllocationHeaderSelect(option)}}
                             placeholder='Select department' 
                             title='Select department' />
                     </div>
                     
-                    { selectedAllocationHeaders && selectedAllocationHeaders.length == 0 && 
+                    { selectedTravelAllocationHeaders && selectedTravelAllocationHeaders.length == 0 && 
                     <div className='mt-6 flex gap-4'>
                         <input type='radio' />
                         <p className='text-zinc-800 text-sm font-medium font-cabin'>Not Sure</p>
                     </div>}
                     
                     
-                    {selectedAllocationHeaders && selectedAllocationHeaders.length>0 &&
+                    {selectedTravelAllocationHeaders && selectedTravelAllocationHeaders.length>0 &&
                         <div className='mt-8'>
                         <p className='text-base font-medium text-neutral-700 font-cabin'>Allocate percentage for selected department</p>
                         <div className='flex justify-between gap-1 items-center flex-wrap'>
-                            {selectedAllocationHeaders && selectedAllocationHeaders.map((item,index)=> 
+                            {selectedTravelAllocationHeaders && selectedTravelAllocationHeaders.map((item,index)=> 
                                 <div className='mt-4 border border-white hover:border-gray-100'>
                                     <InputPercentage 
                                     key={index}
@@ -161,17 +280,109 @@ export default function Page_1(props){
                     </div>}
 
                 </div> }
+                
+                {/* slect team members */}
+                {((MANAGER_FLAG && bookingForTeam) || isDelegatorManager) && <div className='mt-8'>
+                    <div className='flex gap-4 items-center'>
+                        {isDelegatorManager && <Checkbox checked={selectDelegatorTeamMembers} onClick={(e)=>{setSelectDelegatorTeamMembers(e.target.checked)}} />} 
+                        <p className='text-base font-medium text-neutral-700 font-cabin'>{`${isDelegatorManager? `Select ${nameOfDelegator.split(' ')[0]}'s team members for this trip` : 'Select team members for this trip'}`}</p>
+                    </div>
 
+                    {/* table */}
+                   { (selectDelegatorTeamMembers || bookingForTeam) &&  
+                   <div className='mt-4 w-full h-fit max-h-[800px] px-auto py-8 border border-neutral-200 inline-flex rounded-xl justify-center items-center'>
+                        <div className='w-3/4 mx-1/4 flex flex-row justify-between '>
                             
+                            {MANAGER_FLAG && teamMembers &&  !selectDelegatorTeamMembers &&
+                                <>
+                                    <CheckboxCol checked={selectedTeamMembers} onClick={(e, id)=>handleTeamMemberSelect(e, id)} employees={teamMembers}/>
+                                    <EmpIdCol employees={teamMembers} />
+                                    <NameCol employees={teamMembers}/>
+                                    <DesignationCol employees={teamMembers}/>
 
+                                </>
+                            }
 
+                            {isDelegatorManager && delegatorsTeamMembers && selectDelegatorTeamMembers &&
+                            <>
+                                <CheckboxCol checked={selectedTeamMembers} onClick={(e, id)=>handleTeamMemberSelect(e, id)} employees={delegatorsTeamMembers}/>
+                                <EmpIdCol employees={delegatorsTeamMembers} />
+                                <NameCol employees={delegatorsTeamMembers}/>
+                                <DesignationCol employees={delegatorsTeamMembers}/>
+                            </>
+                            }
+                            
+                        </div>
+                    </div>}
+
+                </div>}
+                        
                 <div className='my-8 w-[134px] float-bottom float-right'>
                     <Button 
                         text='Continue' 
                         onClick={handleContinueButton} />
                 </div> 
             </div>
-
-</div>
+        </div>
     </>)
+}
+
+
+
+function CheckboxCol(props){
+    const employees = props.employees
+    const onClick = props.onClick
+    const checked = props.checked || []
+
+    return(
+        <div className='flex-col justify-start items-start gap-6 inline-flex'>
+            <div className="flex-col justify-start items-center gap-2 flex">
+                <div className="max-w-[134px] py-2 h-10 justify-start items-center inline-flex">
+                </div>
+                {employees && employees.map(employee=>
+                    <div className="max-w-[134px] shrink py-2 h-10 justify-start items-center inline-flex">
+                        <Checkbox onClick={(e,id)=>onClick(e, id)} checked={checked.includes(employee.empId)} id={employee.empId} />
+                    </div>)}
+            </div>
+        </div>
+    )
+}
+
+function NameCol(props){
+    const employees = props.employees
+
+    return(
+        <div className='flex-col justify-start items-start gap-6 inline-flex'>
+            <div className="flex-col justify-start items-center gap-2 flex">
+                <TableItem text='Name' header='true' />
+                {employees && employees.map(employee=><TableItem text={employee.name} />)}
+            </div>
+        </div>
+    )
+}
+
+function EmpIdCol(props){
+    const employees = props.employees
+
+    return(
+        <div className='flex-col sr-only md:sr-only lg:not-sr-only justify-start items-start gap-6 inline-flex'>
+            <div className="flex-col justify-start items-center gap-2 flex">
+                <TableItem text='Employee Id' header='true' />
+                {employees && employees.map(employee=><TableItem text={employee.empId} />)}
+            </div>
+        </div>
+    )
+}
+
+function DesignationCol(props){
+    const employees = props.employees
+    console.log('this ran', employees.length)
+    return(
+        <div className='flex-col sr-only md:not-sr-only justify-start items-start gap-6 inline-flex'>
+            <div className="flex-col justify-start items-center gap-2 flex">
+                <TableItem text='Designation' header='true' />
+                {employees && employees.map(employee=><TableItem text={employee.designation} />)}
+            </div>
+        </div>
+    )
 }
