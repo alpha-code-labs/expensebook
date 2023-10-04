5c333f18a4f87fa7e01aa73fa3591d34c8c95191
Ankit Singh <ajayaxes318@gmail.com>
2023-10-04 17:33:22 +0530

    states to store onboarding and form data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,87 @@ import Page_3 from './pages/Page_3'
 import Page_4 from './pages/Page_4'
 
 
-
 function App() {
   
   const TRAVEL_MICROSERVICE_SERVER_URL = "";
+/* 
+  const onBoardingData = {
+    MANAGER_FLAG:true,
+    APPROVAL_FLAG:true,
+    managersList:[],
+    teamMembers:[],
+    tripPurposeOptions:[],
+    delegatedFor:[],
+    travelAllocationHeaderOptions:[],
+    modeOfTransitOptions:[],
+    travelClassOptions:[],
+    hotelClassOptions:[],
+  }
+
+  */
+
+  //dummy onboarding..
+
+  const teamMembers2 = [{name: 'Rajat Rathor', empId: '201', designation: 'Executive'}, {name: 'Ashish Pundir', empId: '205', designation: 'Program Planner'}, {name: 'Ankit Pundir', empId:'209', designation:'Manager'}]
+  const EMPLOYEE_ID  = '123'
+
+  const onBoardingData = {
+    MANAGER_FLAG:true,
+    APPROVAL_FLAG:true,
+    ALLOCATION_HEADER:true,
+    DELEGATED_FLAG:true,
+    managersList:[
+      {name: 'Preeti Arora', empId: '005'},
+      {name: 'Abhas Kamboj', empId: '045'},
+      {name: 'Sandeep Nair', empId: '061'},
+      {name: 'Sumesh', empId: '114'},
+      {name: 'Prabhat', empId: '181'},
+  ],
+    teamMembers:[],
+    tripPurposeOptions:['Meeting with client', 'Sales Trip', 'Business Trip'],
+    delegatedFor:[
+      {name: 'Ajay Singh', empId:'121', group:'', EmpRole:'', teamMembers:[]}, 
+      {name: 'Abhijay Singh', empId:'124', group:'', EmpRole:'', teamMembers:teamMembers2}, 
+      {name: 'Akshay Kumar', empId:'127', group:'', EmpRole:'', teamMembers:[]}, 
+      {name:'Anandhu Ashok K.', empId:'129', group:'', EmpRole:'', teamMembers:[]}, 
+      {name:'kanhaiya', empId:'', group:'135', EmpRole:'', teamMembers:[]}
+  ],
+    travelAllocationHeaderOptions:['Sales', 'Marketing', 'Engineering', 'Research'],
+    modeOfTransitOptions:['Flight', 'Train', 'Bus', 'Cab'],
+    travelClassOptions:{'flight':['Business', 'Economy', 'Premium Economy', 'Private'],
+    'train': ['Sleeper', 'Chair Car', 'First AC', 'Second AC', 'Third AC'],
+    'bus': ['Sleeper', 'Semi-Sleeper', 'Regular'],
+    'cab': ['Sedan', 'Mini']
+   },
+    hotelClassOptions:['4 Star',  '3 Start', '2 Star'],
+    cabClassOptions: ['Sedan', 'Mini'],
+
+  }
+
+
+  const [formData, setFormData] = useState({
+    status: 'draft',
+    state: 'section0',
+    createdBy: EMPLOYEE_ID,
+    createdFor: [],
+    travelAllocationHeaders:[],
+    itinerary: {
+      cities:[{from:null, to:null, departure: {date:null, time:null}, return: {date:null, time:null}}],
+      hotels:[],
+      cabs:[],
+      modeOfTransit:null,
+      travelClass:null,
+      needsVisa:false,
+      needsAirportTransfer:false,
+      needsHotel:false,
+      needsFullDayCabs:false,
+      tripType:{oneWayTrip:true, roundTrip:false, multiCityTrip:false}
+    }
+  })
+
+
+
+
   
   //flags
   
@@ -32,10 +109,10 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Page_1 />} />
-        <Route path='/section0' element={<Page_1 />} />
-        <Route path='/section1' element={<Page_2 />} />
-        <Route path='/section2' element={<Page_3 />} />
-        <Route path='/section3' element={<Page_4 />} />
+        <Route path='/section0' element={<Page_1 formData={formData} setFormData={setFormData} onBoardingData={onBoardingData} />} />
+        <Route path='/section1' element={<Page_2 formData={formData} setFormData={setFormData} onBoardingData={onBoardingData} />} />
+        <Route path='/section2' element={<Page_3 formData={formData} setFormData={setFormData} onBoardingData={onBoardingData} />} />
+        <Route path='/section3' element={<Page_4 formData={formData} setFormData={setFormData} onBoardingData={onBoardingData} />} />
       </Routes>
     </Router>
   </>;
