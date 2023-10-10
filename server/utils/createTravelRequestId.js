1cb8aca221388dcc10bdbf65843f992f4dd49eaf
Ankit Singh <ajayaxes318@gmail.com>
2023-10-10 10:41:06 +0530

    modified to make id based on current time stamp

--- server/utils/createTravelRequestId.js
+++ server/utils/createTravelRequestId.js
@@ -1,11 +1,7 @@
-let incrementalNumber = 0;
 
 export default function createTravelRequestId(tenantId, employeeId) {
-  // Increment the number for each request
-  incrementalNumber++;
 
-  const paddedNumber = String(incrementalNumber).padStart(6, '0');
-  const travelRequestId = `${tenantId}_${employeeId}_tr_${paddedNumber}`
+  const travelRequestId = `${tenantId}_${employeeId}_tr_${Date.now()}`
   return travelRequestId
 }
 
