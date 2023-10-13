8c386d67b06027654e235fdb8cfca95e414c9463
Ankit Singh <ajayaxes318@gmail.com>
2023-10-13 15:01:44 +0530

    changed closing behaviour

--- client/src/components/common/Modal.jsx
+++ client/src/components/common/Modal.jsx
@@ -2,31 +2,24 @@ import { useState, useEffect, useRef } from "react";
 
 export default function Modal(props){
 
-    const [showModal, setShowModal] = useState(true)
     const modalRef = useRef(null)
-    const { children, skipable } = props
-   // const skipable = props.skipable || true
-    console.log(skipable, 'skipable')
+    const [showModal, setShowModal] = [props.showModal, props.setShowModal]
+    const { children, skipable} = props
 
-    useEffect(() => {
-        const handleClick = (event) => {
-          event.stopPropagation()
-          if (skipable && modalRef.current && !modalRef.current.contains(event.target)) {
-              setShowModal(false)
-          }
-        };
-        document.addEventListener("click", handleClick)
-  
-        return () => {
-          console.log('removing dropdown')
-          document.removeEventListener("click", handleClick)
+   // const skipable = props.skipable || true
+    console.log(skipable, showModal, 'skipable')
+
+    const handleOutsideClick = (e)=>{
+        e.preventDefault()
+        e.stopPropagation()
+        if(skipable){
+            setShowModal(false)
         }
-  
-      }, []);
+    }
 
     return(
         <>
-            {showModal && <div className="fixed flex justify-center items-center inset-0 backdrop-blur-sm w-full h-full left-0 top-0 bg-gray-800/60 scroll-none">
+            {showModal && <div onClick={handleOutsideClick} className="fixed overflow-hidden flex justify-center items-center inset-0 backdrop-blur-sm w-full h-full left-0 top-0 bg-gray-800/60 scroll-none">
                 <div ref={modalRef} className='z-10 max-w-[600px] w-[60%] min-h-[200px] scroll-none bg-white rounded-lg shadow-md'>
                     {children}
                 </div>
