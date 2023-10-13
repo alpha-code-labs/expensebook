f892ec87e96faa81831606fc86754455131a50b2
Ankit Singh <ajayaxes318@gmail.com>
2023-10-13 16:47:53 +0530

    fixed formatDate 2

--- client/src/utils/handyFunctions.js
+++ client/src/utils/handyFunctions.js
@@ -66,43 +66,22 @@ function formatDate(date=Date.now()) {
   }
   
   function formatDate2(inputDate) {
-
-    if(isDateInFormat(inputDate)){
-      return inputDate
+    const datePattern = /^\d{4}-\d{2}-\d{2}$/;
+    if(!datePattern.test(inputDate)){
+      return
     }
 
-    const date = new Date(inputDate);
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-US', options);
+    const monthNames = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
   
-    // Extract the day
-    const day = date.getDate();
-  
-    // Add 'th', 'st', 'nd', or 'rd' to the day
-    let daySuffix = '';
-    if (day >= 11 && day <= 13) {
-      daySuffix = 'th';
-    } else {
-      switch (day % 10) {
-        case 1:
-          daySuffix = 'st';
-          break;
-        case 2:
-          daySuffix = 'nd';
-          break;
-        case 3:
-          daySuffix = 'rd';
-          break;
-        default:
-          daySuffix = 'th';
-          break;
-      }
-    }
-  
-    // Construct the final formatted date
-    const finalFormattedDate = `${day}${daySuffix} ${formattedDate}`;
-  
-    return finalFormattedDate;
+    let [year, month, day] = inputDate.split('-')
+    if(day<10) day=day%10
+    const dayWithSuffix = addOrdinalIndicator(day);
+    month = monthNames[month-1]
+
+    return dayWithSuffix + ' ' + month + ' ' + year;
   }
   
 
