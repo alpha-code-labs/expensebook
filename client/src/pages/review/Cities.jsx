8a46e90be9df9cedf4802bd5850dc7bb2efff931
Ankit Singh <ajayaxes318@gmail.com>
2023-10-14 14:59:46 +0530

    changed date formatting function

--- client/src/pages/review/Cities.jsx
+++ client/src/pages/review/Cities.jsx
@@ -1,6 +1,7 @@
 
 import arrows_icon from '../../assets/clarity_two-way-arrows-line.svg'
 import calendar_icon from '../../assets/calendar.svg'
+import { formatDate2 } from "../../utils/handyFunctions"
 
 
 export default function Cities(props){
@@ -19,7 +20,7 @@ export default function Cities(props){
                     <div className="flex items-center gap-1">
                         <img src={calendar_icon} className='w-4 h-4' />
                         <p className='text-neutral-700 text-sm font-cabin'>{formatDate2(item.departure.date)}</p>
-                        {item.return && <> <p className='text-neutral-500 text-sm font-cabin'>to</p>
+                        {item?.return?.date && <> <p className='text-neutral-500 text-sm font-cabin'>to</p>
                         <p className='text-neutral-700 text-sm font-cabin'>{formatDate2(item.return.date)}</p></>}                    
                     </div>
                 </div>
