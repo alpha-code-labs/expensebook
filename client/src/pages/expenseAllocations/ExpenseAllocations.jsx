cd791463e8a42cab84a36ebb903dcfeff0198950
Ankit Singh <ajayaxes318@gmail.com>
2023-10-31 12:01:26 +0530

    component added: for allocation, traval, non-travel and travel-related expenses

---
+++ client/src/pages/expenseAllocations/ExpenseAllocations.jsx
@@ -0,0 +1,58 @@
+import back_icon from '../../assets/arrow-left.svg'
+import Icon from '../../components/common/Icon'
+import HollowButton from '../../components/common/HollowButton';
+import Button from '../../components/common/Button';
+import Modal from '../../components/common/Modal';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function (props) {
+    const [showSkipModal, setShowSkipModal] = useState(false);
+    const navigate = useNavigate();
+    const tenantId = props.tenantId || 'wtobcwyjw';
+
+    return(<>
+        { <div className="bg-slate-50 min-h-[100vh] px-[104px] py-20 w-full">
+            <Icon/>
+            <div className='px-6 py-10 bg-white mt-6 rounded shadow w-full'>
+               
+                {/* rest of the section */}
+                <div className='mt-10 w-full flex flex-col gap-4 text text-2xl font-cabin text-neutral-700 '>  
+                    
+
+                    In this section we will configure how your company allocates travel, 
+                    its related expenses and non travel expenses 
+
+                    
+                    <div className='inline-flex w-full justify-between mt-10'>
+                        <div className='w-[250px]'>
+                            <Button text='Continue' onClick={()=>navigate(`travel`, {state:{tenantId}})} />
+                        </div>
+                        <div className='w-[250px] inline-flex justify-end'>
+                            <HollowButton title='Skip' onClick={()=>setShowSkipModal(true)} />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <Modal showModal={showSkipModal} setShowModal={setShowSkipModal} skipable={true}>
+                <div className='p-10 text text-xl font-cabin'>
+                    Your system will be setup without a way to allocate expenses. You can always configure this section later.
+
+                    <div className='w-[200px] mt-10'>
+                        <Button text='Sure' />
+                    </div>
+                </div>
+                
+            </Modal>
+
+        </div>}
+        
+        </>
+    );
+  }
+
+
+
+
