da0a2b9158f4db3d59a3dfb6557c481555c33f1f
Ankit Singh <ajayaxes318@gmail.com>
2023-10-31 11:59:55 +0530

    changed size of button

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -6,7 +6,7 @@ export default function Button(props){
     return(<>
     <div
         onClick={onClick} 
-        className="w-full h-12 px-8 py-4 bg-indigo-600 rounded-[32px] justify-center items-center gap-2 inline-flex cursor-pointer">
+        className="w-full h-10 px-8 py-4 bg-indigo-600 rounded-[32px] justify-center items-center gap-2 inline-flex cursor-pointer">
         <div className="w-full h-5 whitespace-nowrap text-center text-white text-base font-medium font-cabin">{text}</div>
     </div>
     </>)
