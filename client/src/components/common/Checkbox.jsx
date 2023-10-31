e4946aedfc39bb9d5ac1593e4a0a3100d14db4dd
Ankit Singh <ajayaxes318@gmail.com>
2023-10-31 11:59:31 +0530

    added onchange method, passed in props

--- client/src/components/common/Checkbox.jsx
+++ client/src/components/common/Checkbox.jsx
@@ -3,14 +3,18 @@ export default function Checkbox(props){
     
     const onClick = props.onClick
     const checked = props.checked
+    const onChange = props.onChange || null
     const id = props.id
     const [isChecked, setIsChecked] = useState(false)
+    const handleOnChange = (e) => {
+        setIsChecked(e.target.checked)
+    }
 
     return(<>
         <input 
             type='checkbox'
             onClick={(e)=>onClick(e, id)} 
-            onChange={(e)=>setIsChecked(e.target.checked)}
+            onChange={handleOnChange}
             checked={checked}
             id={id}
             className='w-[18px] h-[18px] cursor-pointer font-thin accent-indigo-600 relative rounded border border-neutral-400' />
