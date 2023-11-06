6f4e540e54e2e5f814cce77451d56046a09e5810
Ankit Singh <ajayaxes318@gmail.com>
2023-11-06 23:36:21 +0530

    updated HR master schema

--- server/model/hr_company_structure.js
+++ server/model/hr_company_structure.js
@@ -1,98 +1,16 @@
 import mongoose from "mongoose"
 
-//  company headquarters
-const companyHeadquartersSchema = new mongoose.Schema({
-  country: String,
-  state: String,
-  city: String,
-})
-
-// Contact Details schema
-const contactSchema = new mongoose.Schema({
-    phoneNumber: String,// here just check phoneNumber and MobileNumber needed
-    mobileNumber: {
-      primary: String,
-      secondary: String,
-    },
-    emailId: String,
-  })
-//basic details schema
-const basicDetailsSchema = new mongoose.Schema({
-  band: String,
-  citizenship: String,
-  costCenterSourceCode: String,
-  designationName: String,
-  designationSourceCode: String,
-  employeeCategory: String,
-  employeeCategorySourceCode: String,
-  grade: String,
-  legalEntity: String,
-  costCenter: String,
-  profitCenter: String,
-  department: String,
-  businessUnit: String,
-  geographicalLocation: String,
-  division: String,
-  project: String,
-  responsibilityCenter: String,
-  joiningDate: Date,
-})
-
-//employee details schema
-const employeeDetailsSchema = new mongoose.Schema({
-  employeeId: String,
-  basicDetails: {}, //for now making it flexible removed basicDetailsSchema
-  //contactDetails: contactSchema, //not needed for now covered in basicDetails
-  //emergencyContactDetails: {
-  //  mobileNumber: {
-  //      primary: String,
-  //    },
-  //},
-  // employmentDetails: {
-  //  companyName: String,
- //   employeeCode: String,
- // },
-  group: String, // employee group name (specific to our system) ? can employee have more than one group
-})
-
-//temporary assigned manager schema
-const temporaryAssignedManagerSchema = new mongoose.Schema({
-  assignedFor: String,
-  approvals: {
-    travelRequest: Boolean,
-    cashAdvance: Boolean,
-    expenseApproval: Boolean,
-  },
-})
-
-// delegated schema
-const delegatedSchema = new mongoose.Schema({
-  delegatedFor: String,
-  endDate: Date,
-})
-
-//reporting schema 
-const reportingSchema = new mongoose.Schema({
-  level: String,
-  managerName: String,
-  managerEmpId: String,
-});
-
-
 // employee schema
 const employeeSchema = new mongoose.Schema({
   employeeDetails: {}, //for now it makes sense to make it flexible, extracting groups tag from here
-  group: String, 
+  group: [String], 
   employeeRoles: {
     employee: Boolean,
     employeeManager: Boolean,
     finance: Boolean,
-    businessAdmin: Boolean,
+    businessAdmin: Boolean,  //change it to travel admin
     superAdmin: Boolean,
   },
-  temporaryAssignedManager: temporaryAssignedManagerSchema,
-  delegated: delegatedSchema,
- // reportingto: [reportingSchema], //not needed for now 
 })
 
 // company details schema
@@ -106,6 +24,11 @@ const companyDetailsSchema = new mongoose.Schema({
   industry: String,
 })
 
+const ExchangeValueSchema = new mongoose.Schema({
+  currencyName: String,
+  value: Number, // Represents the exchange rate for the entered currency
+});
+
 // HR & Company Structure schema
 const hrCompanySchema = new mongoose.Schema({
   tenantId: String,
@@ -116,28 +39,65 @@ const hrCompanySchema = new mongoose.Schema({
   },
   companyDetails: companyDetailsSchema,
   employees: [employeeSchema],
-  /*groups:{
-    groupHeaders: [String],
-    groupName: [String],
-  },*/ // not needed for now will do this in gorups and policy container
+  groups:[{
+    groupName: String,
+    employees: [],
+    canDelegate:[],
+  }],
+  policies:{
+    policyStructure:{},
+    ruleEngine:{}
+  },
   groupHeaders:{
-    grades: [String],
-    bands:[String],
-    designations: [String],
   },
-
   orgHeaders:{
-    //departments: [String],// do we need departmentName also
-    //legalEntities: [String],
-    //costCenters: [String],
-    //profitCenters: [String],
-    //geographicalLocations: [String],
-    //responsibilityCenters: [String],
-    //businessUnits: [String],
-    //divisions: [String],
-    //projects: [String],
   },
-
+  travelAllocation: [{
+    headerName: {
+      type: String,
+    },
+    headerValues: [{
+      type: String,
+    }],
+  }],
+  travelExpenseAllocation: [{
+    headerName: {
+      type: String,
+    },
+    headerValues: [{
+      type: String,
+    }],
+  }],
+  nonTravelExpenseAllocation: [{
+    headerName: {
+      type: String,
+    },
+    headerValues: [{
+      type: String,
+    }],
+  }],
+  groupingLabels: [{
+    headerName: {
+      type: String,
+    },
+    headerValues: [{
+      type: String,
+    }],
+  }],
+  expenseCategories: [{
+    categoryName: String,
+    categoryValues: [String],
+  }],
+  accountLines: [{
+    categoryName: String,
+    accountLine: String,
+  }],
+  multiCurrencyTable: [
+    {
+      currencyName: String,
+      exchangeValue: [ExchangeValueSchema], // Store the exchange rate for each currency
+    },
+  ],
   cashAdvanceOptions: [String], // add options here if its fixed values 
   cashExpenseOptions: [String],// add options here if its fixed values 
 })
@@ -146,11 +106,3 @@ const hrCompanySchema = new mongoose.Schema({
 const HRCompany = mongoose.model('HRCompany', hrCompanySchema)
 
 export default HRCompany
-
-
-
-
-
-
-
-
