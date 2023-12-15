817e4cd89f0fd9cff706f0053d0e911350350389
Ajay Singh <ajayaxes318@gmail.com>
2023-12-15 19:31:49 +0530

    all

--- server/routes/internalRoutes.js
+++ server/routes/internalRoutes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router.get("/travel-requests/", getTravelRequests);
 router.get("/travel-requests/:travelRequestId", getTravelRequest);
 router.get("/travel-requests/:travelRequestId/status", getTravelRequestStatus);
-router.put("/travel-requests/:travelRequestId", updateTravelRequest);
+router.post("/travel-requests/:travelRequestId", updateTravelRequest);
 router.put("/travel-requests/:travelRequestId/status", updateTravelRequestStatus);
 router.post("/onboarding", updateOnboardingContainer)
 export default router;
