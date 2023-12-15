c3760bcd645640a1f6837027b95dd3d706771ddc
Ajay Singh <ajayaxes318@gmail.com>
2023-12-15 19:20:07 +0530

    added routes for cancelling and clearing rejected travel request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,20 @@ import axios from 'axios'
 import "./App.css";
 import CreateTravelRequest from './pages/CreateTravelRequest';
 import ModifyTravelRequest from './pages/ModifyTravelRequest';
+import CancelTravelRequest from './pages/CancelTravelRequest'
+import ClearRejectedTravelRequest from './pages/ClearRejectedTravelRequest';
 import Bookings from './pages/Bookings'
 
-function App() {
-  
 
+function App() {
   return <>
     <Router>
       <Routes>
         <Route path='/create/*' element={<CreateTravelRequest />} />
         <Route path='/modify/:travelRequestId/*' element={<ModifyTravelRequest />} />
         <Route path='/bookings/:travelRequestId' element={<Bookings/>} />
+        <Route path='/cancel/:travelRequestId' element={<CancelTravelRequest/>} />
+        <Route path='/clear-rejected/:travelRequestId' element={<ClearRejectedTravelRequest />} />
       </Routes>
     </Router>
   </>;
