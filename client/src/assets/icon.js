fbbbe52f23e39d813d2d2b393ce4268276fbd856
Ajay Singh <ajayaxes318@gmail.com>
2023-12-15 19:22:28 +0530

    added assets for cancellation form

---
+++ client/src/assets/icon.js
@@ -0,0 +1,36 @@
+export { default as expense_text } from './expensebook.svg';
+export {default as frame_504} from './Frame 504.svg';
+export {default as airplay_1} from './airplay (1).svg';
+export {default as airplay_2} from './airplay (2).svg';
+export {default as airplay} from './airplay.svg';
+export {default as bell} from './bell.svg';
+export {default as logo_with_text} from './logowithtext.svg';
+export {default as arrow_left} from './arrow-left.svg';
+export {default as chevron_down} from './chevron-down.svg';
+export {default as cancel_round} from './cancel-dot.svg';
+export {default as cancel} from './x.svg';
+export {default as airplane} from './Airplane.svg';
+export {default as airplane_1} from './Airplane_1.svg';
+export {default as double_arrow} from './double-arrow.svg';
+export {default as calender} from './calender.svg';
+export {default as location} from './location.svg';
+export {default as cab} from './cab.svg';
+export {default as briefcase} from './briefcase.svg';
+export {default as receipt} from './receipt.svg';
+export {default as money} from './money.svg';
+export {default as house_simple} from './house-simple.svg';
+export {default as intransit_trip} from './intransit-trip.svg';
+export {default as upcoming_trip} from './upcoming-trip.svg';
+export {default as all_trips} from './all-trips.svg';
+export {default as up_arrow} from './up-arrow.svg';
+export {default as down_arrow} from './down-arrow.svg';
+export {default as the_food_bill} from './theFoodBill.png';
+export {default as alert_circle} from './alert-circle.svg';
+export {default as frame_341} from './frame-341.svg';
+export {default as validation_sym} from './validation-sym.svg';
+export {default as three_dot} from './three-dot.png';
+export {default as down_left_arrow} from './down-left-arrow.svg';
+export {default as modify} from './modify.svg'
+export {default as cab_purple} from './cab-purple.svg';
+export {default as train} from './train.png';
+export {default as bus} from './bus.png'
