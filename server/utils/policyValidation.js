33cde469f25086d41c7439b895b946c5a6ee4891
Ajay Singh <ajayaxes318@gmail.com>
2023-12-17 13:51:49 +0530

    minor bug fixes

--- server/utils/policyValidation.js
+++ server/utils/policyValidation.js
@@ -3,27 +3,33 @@ import policies from '../test_data/travelPolicy.json' assert { type: "json" }
 
 export default async function policyValidation(type, groups, policy, value){
     //  console.log(policies)
+    try{
+        
+        console.log(type, groups, policy, value)
+        let notAllowed = false
 
-    console.log(type, group, policy, value)
-    let notAllowed = false
+        groups.forEach(group=>{
+            if(policies['policies'][type][group][policy][value] != undefined){
+                if(policies['policies'][type][group][policy][value].allowed == true){
+                    return {allowed: true, violationMessage:null}
+                }
+                else{
+                    notAllowed = true
+                }
+            }
+        })
 
-    groups.forEach(group=>{
-        if(policies['policies'][type][group][policy][value] != undefined){
-            if(policies['policies'][type][group][policy][value].allowed == true){
-                return {allowed: true, violationMessage:null}
-            }
-            else{
-                notAllowed = true
-            }
+        if(notAllowed){
+            return {allowed:false, violationMessage: policies['policies'][type][groups[0]][policy][value].violationMessage }
         }
-    })
+        
+        //in case there are no policies to verify
+        if(!notAllowed){
+            return {allowed: null, violationMessage: null}
+        }
 
-    if(notAllowed){
-        return {allowed:false, violationMessage: policies['policies'][type][group][policy][value].violationMessage }
-    }
-    
-    //in case there are no policies to verify
-    if(!notAllowed){
-        return {allowed: null, violationMessage: null}
+
+    }catch(error){
+        throw(error)
     }
 }
