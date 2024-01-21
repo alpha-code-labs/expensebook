8d4b06f69fcc0804dbca4dec3fad2dae9aabe928
Ajay Singh <ajayaxes318@gmail.com>
2024-01-21 11:36:24 +0530

    dummy travel objects data

---
+++ client/src/data/dummy.js
@@ -0,0 +1,196 @@
+
+  const dummyCabs = {
+    itineraryId:null,
+    foromId:null,
+    date:null, 
+    class:null, 
+    preferredTime:null, 
+    pickupAddress:null, 
+    dropAddress:null, 
+    violations:{
+      class: null,
+      amount: null,
+    }, 
+    bkd_date:null,
+    bkd_class:null,
+    bkd_preferredTime:null,
+    bkd_pickupAddress:null,
+    bkd_dropAddress:null,
+    bkd_vioilations:{
+        class:String,
+        amount:String,
+    },
+    modified:false, 
+    cancellationDate:null, 
+    cancellationReason:null,
+    status:'draft',
+    bookingDetails:{
+      docURL: null,
+      docType: null,
+      billDetails: {}, 
+    },
+    type:'regular'
+  }
+
+  const dummyFlight = {
+    itineraryId:null,
+    formId:null,
+    date:null,
+    returnDate:null, 
+    travelClass:null, 
+    time:null,
+    returnTime:null, 
+    from:null, 
+    to:null, 
+    isReturnTravel: false,
+    violations:{
+      class: null,
+      amount: null,
+    }, 
+    bkd_date:null,
+    bkd_returnDate:null,
+    bkd_travelClass:null,
+    bkd_time:null,
+    bkd_returnTime:null,
+    bkd_from:null,
+    bkd_to:null,
+    bkd_violations:{
+        class: null,
+        amount: null,
+    },
+    modified:false, 
+    cancellationDate:null, 
+    cancellationReason:null,
+    status:'draft',
+    bookingDetails:{
+      docURL: null,
+      docType: null,
+      billDetails: {}, 
+    },
+    approvers:[],
+    travelAllocations:[],
+  }
+
+  const dummyBus = {
+    itineraryId:null,
+    formId:null,
+    date:null, 
+    travelClass:null, 
+    time:null,
+    from:null, 
+    to:null, 
+    violations:{
+      class: null,
+      amount: null,
+    }, 
+    bkd_date:null,
+    bkd_travelClass:null,
+    bkd_time:null,
+    bkd_from:null,
+    bkd_to:null,
+    bkd_violations:{
+        class: null,
+        amount: null,
+      },
+    modified:false, 
+    cancellationDate:null, 
+    cancellationReason:null,
+    status:'draft',
+    bookingDetails:{
+      docURL: null,
+      docType: null,
+      billDetails: {}, 
+    },
+    approvers:[],
+    travelAllocations:[],
+  }
+
+  const dummyTrain = {
+    itineraryId:null,
+    formId:null,
+    date:null,
+    returnDate:null, 
+    travelClass:null, 
+    time:null,
+    returnTime:null, 
+    from:null, 
+    to:null, 
+    violations:{
+      class: null,
+      amount: null,
+    }, 
+    bkd_date:null,
+    bkd_class:null,
+    bkd_time:null,
+    bkd_from:null,
+    bkd_to:null,
+    bkd_travelClass:null,
+    bkd_violations:{
+        class: null,
+        amount: null,
+      },
+    modified:false, 
+    cancellationDate:null, 
+    cancellationReason:null,
+    status:'draft',
+    bookingDetails:{
+      docURL: null,
+      docType: null,
+      billDetails: {}, 
+    },
+    approvers:[],
+  }
+
+  const dummyHotel = {
+    itineraryId:null,
+    formId:null,
+    location:null,
+    locationPreference:null,
+    class:null, 
+    checkIn:null, 
+    checkOut:null,
+    violations:{
+      class: null,
+      amount: null,
+    }, 
+    bkd_location:null,
+    bkd_class:null,
+    bkd_checkIn:null,
+    bkd_checkOut:null,
+    bkd_violations:{
+      class: null,
+      amount: null,
+    },
+    modified:false,  
+    cancellationDate:null,
+    cancellationReason:null, 
+    status:null, 
+    status:'draft',
+    bookingDetails:{
+      docURL: null,
+      docType: null,
+      billDetails: {}, 
+    },
+    approvers:[]
+  }
+
+  const dummyPersonalVehicle = {
+    itineraryId:null,
+    formId: null,
+    date:null,
+    preferredTime:null,
+    from:null, 
+    to:null, 
+    modified:false, 
+    cancellationDate:null, 
+    cancellationReason:null,
+    status:'draft',
+    bookingDetails:{
+      docURL: null,
+      docType: null,
+      billDetails: {}, 
+    },
+    approvers:[],
+  }
+
+  export {dummyCabs, dummyFlight, dummyBus, dummyTrain, dummyHotel, dummyPersonalVehicle}
