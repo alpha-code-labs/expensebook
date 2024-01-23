a97e61ae999b1159ef986391427548a8a876769a
Ajay Singh <ajayaxes318@gmail.com>
2024-01-23 16:12:28 +0530

    first commit

---
+++ client/src/requirements/Travel/CreateTravelRequest.jsx
@@ -0,0 +1,9 @@
+import { View, Pressable, TouchableOpacity } from "react-native";
+
+export default function(){
+    return(<>
+        <View>
+            <Text>Create Travel Request</Text>
+        </View>
+    </>)
+}
