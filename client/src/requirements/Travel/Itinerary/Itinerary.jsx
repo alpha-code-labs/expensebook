e01069cae253204f9bd579b5e3fc8422af8ab364
Ajay Singh <ajayaxes318@gmail.com>
2024-01-25 18:06:01 +0530

    first commit

---
+++ client/src/requirements/Travel/Itinerary/Itinerary.jsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+export default function({formData, setFormData}){
+
+    return(<>
+        <Text>
+            {formData.travelRequestId}
+        </Text>
+    </>)
+}
