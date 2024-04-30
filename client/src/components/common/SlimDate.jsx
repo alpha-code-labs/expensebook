c2acee620593829657bcb8786770ab4ebe57aac7
Ajay Singh <ajayaxes318@gmail.com>
2024-04-30 12:06:11 +0530

    Modified component to show Date-Month format dates

--- client/src/components/common/SlimDate.jsx
+++ client/src/components/common/SlimDate.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { titleCase, formatDate } from "../../utils/handyFunctions";
+import { titleCase, formatDate, formatDateMonth } from "../../utils/handyFunctions";
 import chevron_down from "../../assets/chevron-down.svg";
 import { formatDate2 } from "../../utils/handyFunctions";
 
@@ -13,6 +13,7 @@ function getCurrentDate(){
 }
 
 export default function SlimDate(props){
+    const format = props.format??'date-month-year';
     const title = props.title || "Title";
     const min = props?.min??0
     let date = props.date 
@@ -43,7 +44,7 @@ export default function SlimDate(props){
     
 
     return(<>
-        <div className="min-w-[200px] w-full md:w-fit max-w-[403px] h-[73px] flex-col justify-start items-start gap-2 inline-flex">
+{format != 'date-month' && <div className="min-w-[200px] w-full md:w-fit max-w-[403px] h-[73px] flex-col justify-start items-start gap-2 inline-flex">
             {/* title */}
             <div className="text-zinc-600 text-sm font-cabin">{title}</div>
 
@@ -65,6 +66,29 @@ export default function SlimDate(props){
                 </div>
             </div>
       </div>
+}
 
+    {format == 'date-month' && <div className="min-w-[136px] w-full md:w-fit max-w-[403px] h-[73px] flex-col justify-start items-start gap-2 inline-flex">
+            {/* title */}
+            <div className="text-zinc-600 text-sm font-cabin">{title}</div>
+
+            {/* input */}
+            <div className="w-full h-full bg-white items-center flex">
+                <div className="text-neutral-700 w-full  h-full text-sm font-normal font-cabin">
+                    <div className=" w-full z-100 relative h-full decoration:none px-6 py-2 border rounded-md border border-neutral-300 inline-flex justify-center items-center cursor-pointer">
+                        <div className="flex relative w-full gap-4 justify-center items-center" >
+                            <input className='slim absolute w-full h-full opacity-0 focus-visible:outline-0 cursor-hover' onChange={handleChange} value={value} min={getDateXDaysAway(Number(min))} type='date'/>
+                            <div className="text-gray-600 bg-white whitespace-nowrap text-base font-medium font-cabin">{formatDateMonth(value)}</div>
+                            <div className="h-6 w-6">
+                                <img src={chevron_down} alt="open" />
+                            </div>
+                            {error.set && <div className="absolute left-0 top-[33px] w-full text-xs text-red-600 font-cabin">
+                                {error.message}
+                            </div>}
+                        </div>        
+                    </div>
+                </div>
+            </div>
+      </div> }
     </>)
 }
