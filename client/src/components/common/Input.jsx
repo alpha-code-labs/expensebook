2a293fff76479b5a00f3cc1da3951e08a37062b8
Ajay Singh <ajayaxes318@gmail.com>
2024-04-30 12:05:11 +0530

    Modified Input component to have a location icon when needed

--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { titleCase } from "../../utils/handyFunctions";
+import { location_icon } from "../../assets/icon";
 
 export default function Input(props){
     const placeholder = props.placeholder || "Placeholder Text";
@@ -7,6 +8,7 @@ export default function Input(props){
     const title = props.title || "Title";
     const onBlur = props.onBlur
     const onChange = props.onChange
+    const showLocationSymbol = props.showLocationSymbol??false;
     const inputRef = useRef(null)
     const [textInput, setTextInput] = useState(value? titleCase(value) : '')
     const error = props.error || {set:false, message:''}
@@ -29,25 +31,25 @@ export default function Input(props){
             onBlur(e)
         }
     }
-
-
-
     
     return(<>
-        <div className="min-w-[200px] w-full md:w-fit max-w-[403px] h-[73px] flex-col justify-start items-start gap-2 inline-flex">
+        <div className={`${showLocationSymbol? 'min-w-[184px]' : 'min-w-[200px]'} w-full md:w-fit max-w-[403px] h-[73px] flex-col justify-start items-start gap-2 inline-flex`}>
             {/* title */}
             <div className="text-zinc-600 text-sm font-cabin">{title}</div>
 
             {/* input */}
             <div className="relative w-full h-full bg-white items-center flex">
-                <div className="text-neutral-700 w-full  h-full text-sm font-normal font-cabin">
+                <div className="relative text-neutral-700 w-full  h-full text-sm font-normal font-cabin">
+                {showLocationSymbol && <img src={location_icon} className="absolute w-4 h-4 top-[15px] left-6"/>}
                     <input
                         ref={inputRef}
                         onChange={handleChange} 
                         onBlur={handleBlur}
-                        className=" w-full h-full decoration:none px-6 py-2 border rounded-md border border-neutral-300 focus-visible:outline-0 focus-visible:border-indigo-600 " 
+                        className={`w-full h-full decoration:none ${showLocationSymbol? 'pr-6 pl-12' : 'px-6'} py-2 border rounded-md border border-neutral-300 focus-visible:outline-0 focus-visible:border-indigo-600 `}
                         value={textInput} 
-                        placeholder={placeholder}></input>
+                        placeholder={placeholder}>
+                            
+                        </input>
                 </div>
 
                 <div className="absolute text-xs text-red-600 left-0 px-6 top-[44px]">
