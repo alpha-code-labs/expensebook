aab02192fd130643bdfa402dcd9619d6c2982d21
Ajay Singh <ajayaxes318@gmail.com>
2024-05-20 12:10:05 +0530

    modified to show error on passing error.set prop as true

--- client/src/components/common/SlimDate.jsx
+++ client/src/components/common/SlimDate.jsx
@@ -75,7 +75,7 @@ export default function SlimDate(props){
             {/* input */}
             <div className="w-full h-full bg-white items-center flex">
                 <div className="text-neutral-700 w-full  h-full text-sm font-normal font-cabin">
-                    <div className=" w-full z-100 relative h-full decoration:none px-6 py-2 border rounded-md border border-neutral-300 inline-flex justify-center items-center cursor-pointer">
+                    <div className={`w-full z-100 relative h-full decoration:none px-6 py-2 border rounded-md border ${error.set? 'border-red-600' : 'border-neutral-300' }  inline-flex justify-center items-center cursor-pointer`}>
                         <div className="flex relative w-full gap-4 justify-center items-center" >
                             <input className='slim absolute w-full h-full opacity-0 focus-visible:outline-0 cursor-hover' onChange={handleChange} value={value} min={getDateXDaysAway(Number(min))} type='date'/>
                             <div className="text-gray-600 bg-white whitespace-nowrap text-base font-medium font-cabin">{formatDateMonth(value)}</div>
