776f3bc65a20c043e5d3fe8c0a7b56c4dbb0fea1
Ajay Singh <ajayaxes318@gmail.com>
2024-05-21 17:39:47 +0530

    changed build directory and server proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  resolve: {
+      alias: {
+          '@': path.resolve(__dirname, './src'),
+      },
+  },
+  server: {
+      proxy: {
+          '/api': {
+              target: 'https://onboarding-server.victoriousplant-d49987f1.centralindia.azurecontainerapps.io/api/',
+              changeOrigin: true,
+          },
+      },
+  },
 })
