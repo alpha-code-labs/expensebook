a9e7d0d2f8916b0fbfc275fc4dcf66949b1f361e
Ajay Singh <ajayaxes318@gmail.com>
2024-05-21 17:06:54 +0530

    added image for error messages

--- client/src/components/common/Error.jsx
+++ client/src/components/common/Error.jsx
@@ -1,13 +1,15 @@
 import spinner_icon  from '../../assets/spinner.gif';
+import fetchError_icon from '../../assets/fetchError.jpg'
 
 export default function ({message=null}){
     return(
-        <div className="w-full h-full flex items-center justify-center mt-10">
+        <div className="w-full h-full flex flex-col items-center justify-center mt-20">
             {!message && <div className='flex items-center jusitfy-center'>
                     <img src={spinner_icon} alt='spinner' className='w-6 h-6' />
                 </div>}    
-            {message && <div className='p-10 border border-gray-300 roundex-xl'>
-                <p className="text-xl text-neutral-700 font-cabin">{message}</p>
+            {message && <div className='flex flex-col items-center justify-center'>
+                <img src={fetchError_icon} className='w-[300px] h-[300px] animate-pulse' />
+                <div className="text-xl text-neutral-700 font-cabin mt-4">{message}</div>
                 </div>}
         </div>)
 }
