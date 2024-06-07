ebfce58326b05a50343c71df988d0d501e59fe39
Ajay Singh <ajayaxes318@gmail.com>
2024-06-07 16:48:01 +0530

    bug fix: renamed asset

--- client/src/assets/icon.js
+++ client/src/assets/icon.js
@@ -4,7 +4,7 @@ export {default as airplay_1} from './airplay (1).svg';
 export {default as airplay_2} from './airplay (2).svg';
 export {default as airplay} from './airplay.svg';
 export {default as bell} from './bell.svg';
-export {default as logo_with_text} from './logowithtext.svg';
+export {default as logo_with_text} from './logoWithText.svg';
 export {default as arrow_left} from './arrow-left.svg';
 export {default as chevron_down} from './chevron-down.svg';
 export {default as cancel_round} from './cancel-dot.svg';
