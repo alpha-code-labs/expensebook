6169c80e01c68438a891d3a54023f63e54976810
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2024-06-16 15:08:57 +0530

    link implented for redirection

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -11,7 +11,7 @@ const Input = ({onChange, placeholder,type,error}) => {
               onChange && onChange(e.target.value);
             }}
             type={type}
-            className={`w-full bg-slate-50 h-full text-neutral-600 decoration:none px-4 py-3 rounded border-[1px] placeholder:text-zinc-400 font-inter placeholder:font-inter font-normal leading-5  ${error? 'border-red-300':'border-slate-300'} focus-visible:outline-0  focus:outline-none  focus: border-red-500`}
+            className={`w-full bg-slate-50 h-full text-neutral-600 decoration:none px-4 py-3 rounded border-[1px] placeholder:text-zinc-400 font-inter placeholder:font-inter font-normal leading-5  ${error? 'border-red-300 focus-visible:border-blue-500':'border-slate-300 focus-visible:border-blue-500'} focus-visible:outline-0   `}
             value={inputValue}
             placeholder={placeholder}
           />
