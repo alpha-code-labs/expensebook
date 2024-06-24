5be7a17d98309a92ae90617c277b05f26afb5d0c
Ajay Singh <ajayaxes318@gmail.com>
2024-06-24 15:06:39 +0530

    changed batch job timing to every 1 second

--- server/scheduler/travel_approvedToNextState.js
+++ server/scheduler/travel_approvedToNextState.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const scheduleTime = process.env.SCHEDULE_TIME??'* * * * *';
+const scheduleTime = process.env.SCHEDULE_TIME??'* * * * * *';
 //basic status UpdateBatchJob function...
 export async function statusUpdateBatchJob(){
     try {
