8246f15f5bae22ea7c6e0e81e64d38b17849d344
sumesh <sumesh9050@gmail.com>
2024-06-26 16:10:20 +0530

    non travel routes updated

--- server/routes/reimbursementRouter.js
+++ server/routes/reimbursementRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getReimbursement, settlementReimbursement, unSettlementReimbursement } from "../controller/reimbursementController.js";
+import { getReimbursement, paidNonTravelExpenseReports, settlementReimbursement, unSettlementReimbursement } from "../controller/reimbursementController.js";
 
 const router = express.Router();
- 
+
 router.get("/find/:tenantId" , getReimbursement);
+
+router.put("/paid/:tenantId/:expenseHeaderId", paidNonTravelExpenseReports)
+
 router.put("/settlement" , settlementReimbursement);
 router.put("/unSettlement" , unSettlementReimbursement);
 export default router;
