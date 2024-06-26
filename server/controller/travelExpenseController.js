e71a721612c262c94104ab6602c4be8648d088c7
sumesh <sumesh9050@gmail.com>
2024-06-26 15:53:42 +0530

    expenseReport status update to paid and paidBy is added

--- server/controller/travelExpenseController.js
+++ server/controller/travelExpenseController.js
@@ -1,26 +1,21 @@
 import Finance from "../models/Finance.js";
 
 
-//All Expense Header Reports with status as pending Settlement or Paid (Full Trip).
+//All Expense Header Reports with status as pending Settlement(Full Trip).
 export const getTravelExpenseData = async(req , res)=>{
     try {
       const {tenantId} = req.params
 
       const status = {
         PENDING_SETTLEMENT:'pending settlement',
-        PAID:'paid'
       }
-
-      const expenseStatus = Object.values(status)
 
         const expenseReportsToSettle = await Finance.find({
           tenantId,
           'tripSchema.travelExpenseData':{
             $elemMatch:{
               'actionedUpon': false,
-              'expenseHeaderStatus':{
-                $in:expenseStatus
-              }
+              'expenseHeaderStatus':status.PENDING_SETTLEMENT
             }
           },
         });
@@ -35,6 +30,79 @@ export const getTravelExpenseData = async(req , res)=>{
     }
 };
 
+//Expense Header Reports with status as pending Settlement updated to paid(Full Trip).
+export const paidExpenseReports = async (req, res) => {
+  const { tenantId, travelRequestId, expenseHeaderId } = req.params;
+  const { paidBy } = req.body;
+
+  console.log("Received Parameters:", { tenantId, travelRequestId, expenseHeaderId });
+  console.log("Received Body Data:", { paidBy });
+
+  if (!tenantId || !travelRequestId || !expenseHeaderId || !paidBy) {
+    return res.status(400).json({ message: 'Missing required field' });
+  }
+
+  const status = {
+    PENDING_SETTLEMENT:'pending settlement'
+  };
+
+  const newStatus ={
+    PAID: 'paid',
+  }
+
+  const filter = {
+    tenantId,
+    travelRequestId,
+    'tripSchema.travelExpenseData': {
+      $elemMatch: {
+        expenseHeaderId,
+        expenseHeaderStatus: status.PENDING_SETTLEMENT,
+        actionedUpon: false
+      }
+    }
+  }
+
+  try {
+    const expenseReport = await Finance.findOne(filter);
+
+    if (!expenseReport) {
+      return res.status(404).json({ message: 'No matching document found' });
+    }
+
+    const expenseHeaderIndex = expenseReport.tripSchema?.travelExpenseData.findIndex(
+      (expense) => JSON.stringify(expense.expenseHeaderId) === JSON.stringify(expenseHeaderId)
+    )
+
+    if (expenseHeaderIndex === -1) {
+      return res.status(404).json({ message: 'No matching cash advance found' });
+    }
+
+    const updateResult = await Finance.updateOne(
+      filter,
+      {
+        $set: {
+          'tripSchema.travelExpenseData.$[elem].paidBy': paidBy,
+          'tripSchema.travelExpenseData.$[elem].actionedUpon': true,
+          'tripSchema.travelExpenseData.$[elem].expenseHeaderStatus': newStatus.PAID
+        }
+      },
+      {
+        arrayFilters: [{ 'elem.expenseHeaderId': expenseHeaderId }],
+        new: true
+      }
+    );
+
+    if (updateResult.modifiedCount === 0) {
+      return res.status(404).json({ message: 'No matching document found for update' });
+    }
+
+    console.log("Update successful:", updateResult);
+    return res.status(200).json({ message: 'Update successful', result: updateResult });
+  } catch (error) {
+    console.error('Error updating cashAdvance status:', error.message);
+    return res.status(500).json({ message: 'Internal server error', error: error.message });
+  }
+};
 
 export const settlementTravelExpenseData = async(req , res)=>{
         try {
@@ -83,5 +151,6 @@ export const unSettlementTravelExpenseData = async(req , res)=>{
          res.status(500).json(error);
        }
 }
+
 
 
