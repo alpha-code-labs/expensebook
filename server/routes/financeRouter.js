caabc747cc6382a17381dcbf996de666a544eea7
sumesh <sumesh9050@gmail.com>
2024-06-26 15:50:44 +0530

    dummy data deleted

--- server/routes/financeRouter.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { financeController } from "../controller/financeController.js";
-const router = Router();
-
-router.post("/post" , financeController);
-export default router;
