99e840fd85f78be97a2f77f6ad7927d16cc35f39
sumesh <sumesh9050@gmail.com>
2024-06-26 15:54:18 +0530

    non travel expense settlement updated

--- server/controller/reimbursementController.js
+++ server/controller/reimbursementController.js
@@ -21,6 +21,73 @@ export const getReimbursement = async(req , res)=>{
 };
 
 
+//Expense Header Reports with status as pending Settlement updated to paid(Non Travel Expense Reports).
+export const paidNonTravelExpenseReports = async (req, res) => {
+  const { tenantId, expenseHeaderId } = req.params;
+  const { paidBy } = req.body;
+
+  console.log("Received Parameters:", { tenantId, expenseHeaderId });
+  console.log("Received Body Data: non travel", { paidBy });
+
+  if (!tenantId || !expenseHeaderId || !paidBy) {
+    return res.status(400).json({ message: 'Missing required field' });
+  }
+
+  const status = {
+    PENDING_SETTLEMENT:'pending settlement' 
+  };
+
+  const newStatus ={
+    PAID: 'paid',
+  }
+
+  const filter = {
+    'reimbursementSchema.tenantId':tenantId,
+    'reimbursementSchema.expenseHeaderId':expenseHeaderId,
+    'reimbursementSchema.expenseHeaderStatus':status.PENDING_SETTLEMENT,
+    'reimbursementSchema.actionedUpon':false
+  }
+
+  try {
+    const expenseReport = await Finance.findOne(filter);
+
+    if (!expenseReport) {
+      return res.status(404).json({ message: 'No matching document found' });
+    }
+
+    const isExpenseHeaderId = expenseReport.reimbursementSchema?.expenseHeaderId === expenseHeaderId
+
+  if (!isExpenseHeaderId) {
+   return res.status(404).json({ message: 'No matching document found' });
+   } else {
+    const updateResult = await Finance.updateOne(
+      filter,
+      {
+        $set: {
+          'reimbursementSchema.paidBy': paidBy,
+          'reimbursementSchema.actionedUpon': true,
+          'reimbursementSchema.expenseHeaderStatus': newStatus.PAID
+        }
+      },
+      {
+        new: true
+      }
+    );
+
+    if (updateResult.modifiedCount === 0) {
+      return res.status(404).json({ message: 'No matching document found for update' });
+    }
+
+    console.log("Update successful:", updateResult);
+    return res.status(200).json({ message: 'Update successful', result: updateResult });
+    }
+
+  } catch (error) {
+    console.error('Error updating non travel expense report status:', error.message);
+    return res.status(500).json({ message: 'Internal server error', error: error.message });
+  }
+};
+
 
 export const settlementReimbursement = async(req , res)=>{
 // console.log("LINE AT 15" , req.body);
