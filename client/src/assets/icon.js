962483afcbf020c7bc7e94a5109570047a7d430d
Ajay Singh <ajayaxes318@gmail.com>
2024-07-05 16:30:06 +0530

    added assets

--- client/src/assets/icon.js
+++ client/src/assets/icon.js
@@ -14,6 +14,12 @@ export {default as scanner_icon} from './scanner.svg'
 export {default as clock_icon} from './clock.svg'
 export {default as add_icon} from './add.svg' 
 export {default as left_arrow_icon} from './arrow-left.svg'
+export {default as edit_icon} from './edit.svg'
+export {default as delete_icon} from './delete.svg'
+export {default as straight_arrow_icon} from './arrow_straight.svg'
+export {default as smoking_icon} from './smoking.svg'
+export {default as no_smoking_icon} from './no_smoking.svg'
+export {default as restraunt_icon} from './restraunt.svg'
 
 export {default as material_flight_black_icon} from './material-icons/outline_flight_takeoff_black_48dp.png'
 export {default as material_flight_white_icon} from './material-icons/outline_flight_takeoff_white_48dp.png'
@@ -31,72 +37,5 @@ export {default as material_hotel_black_icon} from './material-icons/outline_hot
 export {default as material_hotel_white_icon} from './material-icons/outline_hotel_white_48dp.png'
 export {default as material_arrow_forward_white_icon} from './material-icons/outline_arrow_forward_white_48dp.png'
 export {default as minus_icon} from './minus.svg'
+export {default as hotel_class_icon} from './hotel_class.svg'
 //export {default as alert_circle_icon} from './alert-circle.svg';
-
-
-
-
-function getFinalSchedule(slotWiseData){
-	let priorityQ = [
-		{combination1:0}, 
-		{combination2:0}, 
-		{combination3:0}, 
-		{combination4:0}];
-	
-	const days = ['saturday', 'sunday'];
-	const finalSchedule = JSON.parse(JSON.stringify(slotWiseData));
-	
-	for(day of days){
-		for(slot in slotWiseData[day]){
-			finalSchedule[day][slot] = [];
-		}
-	}
-	
-	
-	
-	for(day of days){
-		for(slot in slotWiseData[day]){
-			const countOfCombinations = slotWiseData[day][slot].length;
-			
-			if(countOfCombinations < 3){
-				//push all combinations to final schedule
-				finalSchedule[day][slot] = slotWiseData[day][slot];
-				
-				//populate priorityQ
-				slotWiseData[day][slot].forEach(c=>{
-					priorityQ[c] += 1;
-				})
-				
-				priorityQ.sort((a,b)=>{
-					 const aValue = Object.values(a)[0];
-					const bValue = Object.values(b)[0];
-					return aValue - bValue; 
-				})
-			} 
-			else{
-				//we need to select 2 of the given combinations
-				//lowest priority combinations should be given priority
-				
-				let totalSelected = 0;
-				
-				while(totalSelected < 2){
-					for(c in priorityQ){
-						const ind = slotWiseData[day][slot].findIndex(pm=>pm.combination == c);
-						if(ind != -1){
-							finalSchedule[day][slot] = [...finalSchedule[day][slot], slotWiseData[ind]]
-							priorityQ[c] += 1;
-							totalSelected++;
-							break;
-						}
-					}
-					
-					priorityQ.sort((a,b)=>{
-						 const aValue = Object.values(a)[0];
-						const bValue = Object.values(b)[0];
-						return aValue - bValue; 
-					})
-				}
-			}
-		}
-	}	
-}
