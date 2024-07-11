234b87da0a61d9934d20bf1df5504f80dc739b59
sumesh <sumesh9050@gmail.com>
2024-07-11 18:34:11 +0530

    cash

--- server/models/cashSchema.js
+++ server/models/cashSchema.js
@@ -43,7 +43,7 @@ export const cashAdvanceSchema = new mongoose.Schema({
       },
       travelRequestId: {
         type: String,
-        required: true,
+        // required: true,
       },
       travelRequestNumber:{
         type: String,
