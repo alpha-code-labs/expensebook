0137705161c59967d62356a78018fefc77e5e13a
sumesh <sumesh9050@gmail.com>
2024-07-11 10:51:52 +0530

    get/update profile travel preference

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,69 +1,102 @@
+import Joi from "joi";
 import HRMaster from "../models/hrMasterSchema.js";
 import { sendToOtherMicroservice } from "../rabbitmq/publisher.js";
 
 
-// To get employee preferences
-export const getEmployeePreferencesController = async (req, res) => {
+const preferenceSchema = Joi.object({
+    tenantId: Joi.string().required(),
+    empId:Joi.string().required()
+});
+
+// To get employee preferences for a profile
+export const getProfile = async (req, res) => {
     try {
-      // Input validation
-      const { tenantId, empId } = req.params;
-  
-      // Check if required fields are present
-      if (!tenantId || !empId) {
-        return res.status(400).json({ error: 'Missing required fields in the request parameters' });
-      }
-  
+
+    const {error , value } = preferenceSchema.validate(req.params)
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const { tenantId, empId} = value;
+
       // Get employee preferences using the service function
-      const preferences = await HRMaster.findOne(tenantId, empId);
-  
+    const hrData = await HRMaster.findOne({
+        tenantId, 
+        'employees.employeeDetails.employeeId':empId
+    });
+
+    const profile = hrData.employees
+    .filter(employee => employee?.employeeDetails?.employeeId === empId)    
+    .map(employee => ({
+    travelPreferences: employee?.travelPreferences ?? '',
+    imageUrl:employee?.imageUrl ?? '',
+    employeeName: employee?.employeeDetails?.employeeName ?? '',
+    emailId: employee?.employeeDetails?.emailId ?? '',
+    department : employee?.employeeDetails?.department ?? '',
+    location: employee?.employeeDetails?.geographicalLocation ?? '',
+    }))[0]
+
+    const { travelPreferences , imageUrl, employeeName, emailId, department , location} = profile
+    const {flightPreference, busPreference,trainPreference, hotelPreference, emergencyContact, dietaryAllergy, } = travelPreferences
+
       // Send a standardized success response with the retrieved preferences
-      res.status(200).json({ success: true, preferences });
+    res.status(200).json({ success: true, flightPreference, busPreference,trainPreference, hotelPreference, emergencyContact, dietaryAllergy, imageUrl,employeeName, location ,emailId,department });
     } catch (error) {
       // Handle errors in a standardized way
-      console.error('Error getting employee preferences:', error);
-      res.status(500).json({ error: 'Internal server error' });
+    console.error('Error getting employee preferences:', error);
+    res.status(500).json({ error: 'Internal server error' });
     }
 };
-  
+
 
 //save employee preference 
-export const saveEmployeePreferencesController = async (req, res) => {
+export const saveProfile = async (req, res) => {
     try {
-      const { tenantId, empId } = req.params;
-  
-      if (!tenantId || !empId) {
-        return res.status(400).json({ error: 'Missing required fields in the request parameters.' });
-      }
-  
-      const employee = await HRMaster.findOne({
-        'tenantId': tenantId,
-        'employeeDetails.employeeId': empId,
-      });
-  
-      if (!employee) {
-        return res.status(404).json({ error: 'Employee not found.' });
-      }
-  
-      const { busPreference, dietaryAllergy, emergencyContact, flightPreference, hotelPreference, trainPreference } = req.body;
-  
-      if (busPreference) employee.travelPreferences.busPreference = busPreference;
-      if (dietaryAllergy) employee.travelPreferences.dietaryAllergy = dietaryAllergy;
-      if (emergencyContact) employee.travelPreferences.emergencyContact = emergencyContact;
-      if (flightPreference) employee.travelPreferences.flightPreference = flightPreference;
-      if (hotelPreference) employee.travelPreferences.hotelPreference = hotelPreference;
-      if (trainPreference) employee.travelPreferences.trainPreference = trainPreference;
-  
-      await employee.save();
+    
+ const { error, value} = preferenceSchema.validate(req.params)
 
-      const payload = employee.travelPreferences;
-      const action = 'profile-update';
-      const comments = 'travel preference updated by employee in dashboard'
-      sendToOtherMicroservice(payload, action, 'travel', comments, source='dashboard', onlineVsBatch='online')
-      sendToOtherMicroservice(payload, action, 'cash', comments, source='dashboard', onlineVsBatch='online')
-      sendToOtherMicroservice(payload, action, 'approval', comments, source='dashboard', onlineVsBatch='online')
-      sendToOtherMicroservice(payload, action, 'trip', comments, source='dashboard', onlineVsBatch='online')
-      sendToOtherMicroservice(payload, action, 'expense', comments, source='dashboard', onlineVsBatch='online')
-      sendToOtherMicroservice(payload, action, 'finance', comments, source='dashboard', onlineVsBatch='online')
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message
+        });
+    }
+
+    const { tenantId, empId } = value
+
+    const hrData = await HRMaster.findOne({
+        'tenantId': tenantId,
+        'employees.employeeDetails.employeeId': empId,
+    });
+
+    if (!hrData) {
+        return res.status(404).json({ error: 'Employee not found.' });
+    }
+
+    const profile = hrData.employees
+    .find(employee => employee?.employeeDetails?.employeeId === empId)    
+
+
+    const { busPreference, dietaryAllergy, emergencyContact, flightPreference, hotelPreference, trainPreference , imageUrl } = req.body;
+
+      if (busPreference) profile.travelPreferences.busPreference = busPreference;
+      if (dietaryAllergy) profile.travelPreferences.dietaryAllergy = dietaryAllergy;
+      if (emergencyContact) profile.travelPreferences.emergencyContact = emergencyContact;
+      if (flightPreference) profile.travelPreferences.flightPreference = flightPreference;
+      if (hotelPreference) profile.travelPreferences.hotelPreference = hotelPreference;
+      if (trainPreference) profile.travelPreferences.trainPreference = trainPreference;
+      if (imageUrl) profile.imageUrl = imageUrl;
+  
+    await hrData.save();
+
+    //   const payload = employee.travelPreferences;
+    //   const action = 'profile-update';
+    //   const comments = 'travel preference updated by employee in dashboard'
+    //   sendToOtherMicroservice(payload, action, 'travel', comments, source='dashboard', onlineVsBatch='online')
+    //   sendToOtherMicroservice(payload, action, 'cash', comments, source='dashboard', onlineVsBatch='online')
+    //   sendToOtherMicroservice(payload, action, 'approval', comments, source='dashboard', onlineVsBatch='online')
+    //   sendToOtherMicroservice(payload, action, 'trip', comments, source='dashboard', onlineVsBatch='online')
+    //   sendToOtherMicroservice(payload, action, 'expense', comments, source='dashboard', onlineVsBatch='online')
+    //   sendToOtherMicroservice(payload, action, 'finance', comments, source='dashboard', onlineVsBatch='online')
 
       // Send a standardized success response
       res.status(200).json({ success: true, message: 'Employee preferences saved successfully.' });
@@ -73,8 +106,7 @@ export const saveEmployeePreferencesController = async (req, res) => {
       res.status(500).json({ error: 'Internal server error.' });
     }
 };
-  
-  
+
 
 
 
--- server/routes/mainFrontendRoutes.js
+++ server/routes/mainFrontendRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { roleBasedRouter } from './roleBasedRoutes.js';
 import { travelAdminRoutes } from './travelAdmin.js';
+import { profileRouter } from './profileRoutes.js';
 
 export const mainRouter = express.Router();
 
 mainRouter.use("/role", roleBasedRouter);
 
 mainRouter.use("/travel-admin", travelAdminRoutes)
+
+mainRouter.use("/profile", profileRouter)
