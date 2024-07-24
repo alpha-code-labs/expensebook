95a9aff27ecaba32e25296c1185b7d0563c1fa89
sumesh <sumesh9050@gmail.com>
2024-07-24 18:25:57 +0530

    add modify trips

--- server/controllers/modifyTrips.js
+++ server/controllers/modifyTrips.js
@@ -1,5 +1,4 @@
 import Joi from "joi"
-import HRMaster from "../models/hrCompanySchema.js"
 import Trip from "../models/tripSchema.js"
 import { fetchOnboardingData } from "../services/hrData.js"
 import { addALeg } from "./addALeg.js"
@@ -8,9 +7,9 @@ import { itineraryLineItem } from "./cancelTripController.js"
 
 export async function getHrData(req,res) {
     try{
- const { tenantId, empId, travelType} = req.params
+const { tenantId, empId, travelType} = req.params
 
-    const report = await fetchOnboardingData(tenantId,empId)
+    const report = await fetchOnboardingData(tenantId,empId,travelType)
 
     if(report){
         return res.status(200).json(report)
@@ -22,6 +21,29 @@ export async function getHrData(req,res) {
     }
 }
 
+export async function getTrip(tenantId,empId,tripId){
+    try{
+        const getTripDetails =  await Trip.findOne({
+            tenantId,
+            "tripStatus": { $in: ['transit', 'upcoming'] },
+            "tripId": tripId,
+            $or: [
+              { 'travelRequestData.createdBy.empId': empId},
+              { 'travelRequestData.createdFor.empId': empId},
+            ],
+          });
+
+          if(getTripDetails){
+            return getTripDetails
+          } else {
+            throw new Error('Invalid query:Trip Not Found')
+          }
+    } catch (error){
+        console.log(error)
+        throw error
+    }
+}
+
 export const employeeSchema = Joi.object({
     tenantId: Joi.string().required(),
     empId:Joi.string().required(),
@@ -29,64 +51,77 @@ export const employeeSchema = Joi.object({
 });
 
 const itineraryItemSchema = Joi.object({
-    bkd_date: Joi.date(),
-    itineraryId: Joi.string().required()
-  });
+});
 
-  const itinerarySchema = Joi.object({
-    flights: Joi.array().items(itineraryItemSchema),
-    hotels: Joi.array().items(itineraryItemSchema),
-    cabs: Joi.array().items(itineraryItemSchema),
-    buses: Joi.array().items(itineraryItemSchema),
-    trains: Joi.array().items(itineraryItemSchema)
-  });
+const itinerarySchema = Joi.object({
+    newFlights: Joi.array(),
+    newHotels: Joi.array(),
+    newCabs: Joi.array(),
+    newBuses: Joi.array(),
+    newTrains: Joi.array(),
+    newCarRentals: Joi.array(),
+    newPersonalVehicles: Joi.array()
+});
 
 const bodySchema = Joi.object({
     cancelIds: Joi.array().items(Joi.string()),
-    itinerary: itinerarySchema.min(1)
-  }).or('cancelIds', 'itinerary');
+    modifyIds: Joi.array().items(Joi.string()),
+    newItinerary: itinerarySchema.min(1),
+    allocations: Joi.array()
+}).or('cancelIds','allocations','modifyIds','newItinerary')
+
 
 export const modifyTrip = async(req,res) => {
     try{    
         const { error: errorParams, value: valueParams} = employeeSchema.validate(req.params)
         if(errorParams) return res.status(400).send(errorParams.details[0].message)
         const {error: errorBody, value: valueBody} = bodySchema.validate(req.body)
-        if(errorValue) return res.status(400).send(errorBody.details[0].message)
+        if(errorBody) return res.status(400).send(errorBody.details[0].message)
         
         const { tenantId, empId, tripId} = valueParams
-        const {cancelIds , itinerary} = valueBody
+        const {cancelIds=[] , modifyIds=[], newItinerary={}, allocations=[]} = valueBody
 
-        let action;
+        const isValidItinerary = Object.values(newItinerary)
+        .filter(Array.isArray)
+        .some(array => array?.length > 0)
+
+        const isModifyIds = Array.isArray(modifyIds)?.length
+        console.log("here", tenantId, empId, tripId)
+        console.log("cancelIds", JSON.stringify(cancelIds, '' , 2) ,
+        "newItinerary",JSON.stringify(newItinerary, '' , 2),
+        "allocations", JSON.stringify(allocations, '' , 2) )
+
+        const tripDetails = await  getTrip(tenantId,empId,tripId)
+
+        if(tripDetails){
+
         if(cancelIds){
-            action = 'cancelIds'
-        } else if(itinerary){
-            action = 'itinerary'
+            const cancelItinerary = await itineraryLineItem(tripDetails, cancelIds);
+            console.log("cancelItinerary 0 flightzzzz", JSON.stringify(cancelItinerary.travelRequestData.itinerary.flights, " ", 2))
         } 
 
-        const getTrip = Trip.findOne({
-            tripId,
-            tenantId,
-            'travelRequestData.createdBy.empId':empId
-        })
-
-        if(getTrip){
-        switch (action){
-            case 'cancelIds':
-                    const cancelItinerary = await itineraryLineItem(getTrip, cancelIds);
-                    console.log("cancelItinerary", cancelItinerary)
-                
-            break;
-            case 'itinerary':
-                  const modifyTrip = await addALeg(tenantId,empId,tripId,itinerary)
-                  console.log("modifyTrip", modifyTrip)
-            break;
+        if(isValidItinerary && isModifyIds){
+            const modifyTrip = await addALeg(tripDetails,newItinerary,allocations)
+            await itineraryLineItem(tripDetails, modifyIds);
+            console.log("addALeg maga", modifyTrip)
         }
+
+        return res.status(200).json({success:true , tripDetails})
+        } else {
+            return res.status(400).json({success:false, message:"Invalid request:"})
         }
 
     } catch(error){
         console.error(error)
+        return res.status(500).json({success:false, error})
     }
 }
+
+
+
+
+
+
 
 
 
