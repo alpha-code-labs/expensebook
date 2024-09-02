ae2ee38f860eb573ca6b7fb13d3ebf7e95a67fa0
sumesh <sumesh9050@gmail.com>
2024-09-02 19:34:57 +0530

    add: finance update after travel expense report is approved

--- expense/server/rabbitmq/messageProcessor.js/finance.js
+++ expense/server/rabbitmq/messageProcessor.js/finance.js
@@ -1,32 +1,36 @@
 import Expense from "../../models/tripSchema.js";
 
 //travel expense header 'paid'
+// at line item level also update status of lineItem as paid.
 export const settleExpenseReport= async (payload) => {
-    try {
-      const { tenantId, expenseHeaderId, tripId, paidBy, } = payload;
+  try {
+      const {  tenantId,travelRequestId, expenseHeaderId, settlementBy, expenseHeaderStatus, 
+        settlementDate } = payload;
   
-      const trip = await Expense.findOneAndUpdate(
-        { 
-          'tenantId': tenantId,
-           
-          'travelExpenseData': { $elemMatch: { 'tripId': tripId, 'expenseHeaderId': expenseHeaderId } }
-        },
-        { 
-          $set: { 
-            'travelExpenseData.$.expenseHeaderStatus': 'paid', 
-            'travelExpenseData.$.paidBy': paidBy ?? '', 
-          }
-        },
-        { new: true }
-      );
-  
-      console.log('Travel request status updated in approval microservice:', trip);
-      return { success: true, error: null };
-    } catch (error) {
-      console.error('Failed to update travel request status in approval microservice:', error);
-      return { success: false, error: error };
-    }
+    const trip = await Expense.findOneAndUpdate(
+      { 
+        'tenantId': tenantId,
+         
+        'travelExpenseData': { $elemMatch: { travelRequestId, expenseHeaderId } }
+      },
+      { 
+        $set: { 
+          'travelExpenseData.$.expenseHeaderStatus': expenseHeaderStatus, 
+          'travelExpenseData.$.settlementDate': settlementDate,
+          'travelExpenseData.$.settlementBy': settlementBy,
+        }
+      },
+      { new: true }
+    );
+
+    console.log('Travel request status updated in approval microservice:', trip);
+    return { success: true, error: null };
+  } catch (error) {
+    console.error('Failed to update travel request status in approval microservice:', error);
+    return { success: false, error: error };
+  }
 };
+
 
 export const settleExpenseReportPaidAndDistributed= async (payload) => {
     try {
