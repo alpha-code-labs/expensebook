b57c37808241e9ddbe3d21e343e5864ddd89bbb6
sumesh <sumesh9050@gmail.com>
2024-09-25 17:14:18 +0530

    seperated reimbursement into a separate collection/schema

--- server/models/dashboardSchema.js
+++ server/models/dashboardSchema.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import { travelRequestSchema } from "./travelSchema.js";
 import { cashAdvanceSchema } from "./cashSchema.js";
-import { reimbursementSchema } from "./reimbursementSchema.js";
 import { tripSchema } from "./tripSchema.js";
   
 const dashboardSchema = new mongoose.Schema({
@@ -19,16 +18,8 @@ const dashboardSchema = new mongoose.Schema({
     },
     travelRequestId: {
       type: mongoose.Schema.Types.ObjectId,
-      unique: function() {
-        return !this.reimbursementSchema; // Ensure uniqueness only if reimbursementSchema is not present
-      },
-      required: function() {
-        return !this.reimbursementSchema; // Make required only if reimbursementSchema is not present
-      },
-    },
-    reimbursementSchema: {
-      type: reimbursementSchema,
-      required: false, // Make reimbursementSchema optional
+      // unique: true,
+      required: true
     },
     travelRequestSchema:travelRequestSchema,
     cashAdvanceSchema: cashAdvanceSchema,
--- server/models/reimbursementSchema.js
+++ server/models/reimbursementSchema.js
@@ -197,6 +197,9 @@ expenseSettledDate: Date,
 },{timestamps:true});
 
 
+const REIMBURSEMENT = mongoose.model('reimbursements', reimbursementSchema)
+
+export default REIMBURSEMENT
 // Pre hook to generate and assign an ObjectId to expenseHeaderId before saving the document
 reimbursementSchema.pre('validate', function(next) {
   if(!this.expenseHeaderId) {
