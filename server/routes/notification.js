0df3f7598830a685377eed48d603c61b6773fae5
sumesh <sumesh9050@gmail.com>
2024-10-26 15:30:48 +0530

    Add:notification routes added and conscheduler to update notification cron job

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -3,4 +3,4 @@ import { markMessageAsRead } from "../controllers/notificationController.js";
 
 export const notificationRouter = express.Router()
 
-notificationRouter.patch('/:tenantId/:empId/:travelRequestId/:notificationId',markMessageAsRead)
+notificationRouter.patch('/:tenantId/:empId/read',markMessageAsRead)
