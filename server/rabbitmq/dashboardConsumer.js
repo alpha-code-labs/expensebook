baa18b5308fd0c50032c78786c25de6bafaf0f9a
sumesh <sumesh9050@gmail.com>
2024-11-25 00:25:26 +0530

    add: common rabitmq conncetion added and error handling added

--- server/rabbitmq/dashboardConsumer.js
+++ server/rabbitmq/dashboardConsumer.js
@@ -1,115 +1,18 @@
-import amqp from 'amqplib';
 import { fullUpdateTravel } from './messageProcessor/travel.js';
 import dotenv from 'dotenv';
 import { updateTripStatus } from './messageProcessor/trip.js';
 import { expenseReportApproval, travelStandAloneApproval, travelWithCashApproval, travelWithCashTravelApproval } from './messageProcessor/approval.js';
 import { fullUpdateCash } from './messageProcessor/cash.js';
+import { getRabbitMQConnection } from './connection.js';
 
 dotenv.config();
 
-// export const consumeFromDashboardQueue = async () => {
-//   console.log("sync - consumeFromDashboardQueue", consumeFromDashboardQueue)
-//   const rabbitMQUrl = process.env.rabbitMQUrl;
-
-//   try {
-//     console.log('Consuming messages from RabbitMQ...consumeFromDashboardQueue');
-
-//     const connectToRabbitMQ = async () => {
-//       try {
-//         console.log('Connecting to RabbitMQ...');
-//         const connection = await amqp.connect(rabbitMQUrl);
-//         const channel = await connection.createConfirmChannel();
-//         console.log('Connected to RabbitMQ.');
-//         return channel;
-//       } catch (error) {
-//         console.log('Error connecting to RabbitMQ:', error);
-//         throw error;
-//       }
-//     };
-
-//     const channel = await connectToRabbitMQ();
-//     const queueName = 'sync';
-
-//     console.log(`Asserting queue: ${queueName}`);
-//     await channel.assertQueue(queueName, { durable: true });
-
-//     console.log(`Consuming messages from queue: ${queueName}`);
-//     channel.consume(queueName, async (msg) => {
-//       try {
-//         if (msg !== null && msg.content) {
-//           const content = JSON.parse(msg.content.toString());
-
-//           console.log(`coming from ${content.headers?.source}`);
-//           const payload = content?.payload;
-//           const source = content.headers.source;
-//           const isNeedConfirmation = content.headers.needConfirmation;
-
-//           if (isNeedConfirmation) {
-//             // Update dashboard and send confirmation
-//             if(source == 'travel'){
-//               const success = await updateDashboard(payload);
-
-//               if (success) {
-//                 // Send confirmation
-//                 const confirmationMessage = {
-//                   headers: {
-//                     type: 'confirmation',
-//                     correlationId: msg.properties.correlationId,
-//                   },
-//                   payload: { confirmation: true },
-//                 };
-//                 channel.publish('', queueName, Buffer.from(JSON.stringify(confirmationMessage)), {
-//                   persistent: true,
-//                 });
-  
-//                 // Acknowledge the message after successful processing
-//                 channel.ack(msg);
-//               } else{
-//                 console.log("Error processing confirmation message", msg);
-//               }
-//             } else if(source == 'trip'){
-//               if(action == 'status-update'){
-//                 const success = await updateTripStatus(payload);
-//                 if(!success){
-//                   console.log("trip Status updated via batchJob", success)
-//                   const confirmationMessage={
-//                     headers:{
-//                       type: 'confirmation',
-//                       correlationId: msg.properties.correlationId,
-//                     },
-//                     payload:{confirmation: true},
-//                   };
-//                   channel.publish('', queueName, Buffer.from(JSON.stringify(confirmationMessage)), {
-//                     persistent: true,
-//                   });
-//                 // Acknowledge the message after successful processing
-//                 channel.ack(msg);
-//                 } else{
-//                   console.log("Acknowledge message failed from trip ", msg);
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       } catch (error) {
-//         console.error('Error processing message:', error);
-//       }
-//     });
-//   } catch (error) {
-//     console.log('Error consuming messages from RabbitMQ:', error);
-//     throw error;
-//   }
-// };
-
-   
 export const consumeFromDashboardQueue = async () => {
   console.log("sync - consumeFromDashboardQueue");
-  const rabbitMQUrl = process.env.rabbitMQUrl;
- 
   try {
      console.log('Connecting to RabbitMQ...');
-     const connection = await amqp.connect(rabbitMQUrl);
-     const channel = await connection.createConfirmChannel();
+     const connection = await getRabbitMQConnection();
+     const channel = await connection.createChannel();
      console.log('Connected to RabbitMQ.');
      const exchangeName = 'amqp.dashboard';
      const queue = 'sync';
@@ -203,12 +106,3 @@ export const consumeFromDashboardQueue = async () => {
      throw error;
   }
  };
-
-
-
-  const updateDashboard = async (payload) => {
-    return true; // Return true for successful update, false otherwise
-};
-  
-
-
